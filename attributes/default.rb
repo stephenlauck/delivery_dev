bff7b3bdeffd10ceef14c9731bfd180dda1963a7
Jon Morrow <jon@morrowmail.com>
2016-02-18 22:05:14 -0800

    Auto setup lsyncd.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default['delivery']['fqdn'] = "delivery.example.com"
+
+## Setting this to true so lysnc d will retry unitl secondary comes up.
+include_attribute "lsyncd"
+default['lsyncd']['insist'] = true
