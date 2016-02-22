d56392ab09f9fcd859d1a73b830e2b99df07d6ce
Jon Morrow <jon@morrowmail.com>
2016-02-22 10:37:20 -0800

    Postgres cluster

--- delivery_ha.md
+++ delivery_ha.md
@@ -1,3 +1,27 @@
+Delivery Dev Replication cluster
+====
+If you run kitchen converge <<platform>> you should get a cluster with a chef server,
+2 delivery servers, and a builder. The Delivery servers will be setup with the primary's
+postgres doing streaming replication to the secondary's read only data base. Because the
+secondary's database is read only it does not support loging in directly (since delivery
+does a write of the token on login). Never fear the tokens form the primary are sync'd to
+the secondary and there is a work around in the demo steps below to copy this token between
+browser sessions.
+
+
+To test that replication is working:
+1. `kitchen exec delivery-primary-cent -c 'cat /tmp/example.creds'`
+2. log into the primary (https://delivery-primary.example.com/e/example/#) and create an org and/or
+project.
+3. Open the developer tools in chrome
+4. Open 'Resources' and select 'Local Storage'
+5. In another browser window go to the login page of the secondary (https://delivery-secondary.example.com/e/example/#)
+6. Open the developer tools in chrome
+7. Open 'Resources' and select 'Local Storage'
+8. Copy the key 'examplesession' and value from the primary to the secondary browser
+9. Refresh the secondary browser and you should land on the dashboard
+10. From here you can navigate around and see your org/project etc. Just remember only read ops work.
+
 What are the things we need to sync?
 
 * -postgresql
---
+++ recipes/delivery_ha.rb
@@ -0,0 +1,104 @@
+# set up shared delivery keys so the two delivery servers can rsync
+directory '/opt/delivery/embedded/.ssh' do
+  action :create
+  owner 'delivery'
+  group 'delivery'
+  mode '0700'
+end
+
+cookbook_file '/opt/delivery/embedded/.ssh/insecure_private_key' do
+  action :create
+  owner 'delivery'
+  group 'delivery'
+  mode '0600'
+  source 'insecure_private_key'
+end
+
+cookbook_file '/opt/delivery/embedded/.ssh/authorized_keys' do
+  action :create
+  owner 'delivery'
+  group 'delivery'
+  mode '0600'
+  source 'insecure_public_key'
+end
+
+package "rsync"
+
+if node['delivery']['fqdn'] == 'delivery-primary.example.com'
+  # Create replication user.
+  execute "/opt/delivery/embedded/bin/psql -c \"CREATE ROLE replication WITH REPLICATION PASSWORD 'password' LOGIN\" -d delivery chef-pgsql" do
+    not_if "/opt/delivery/embedded/bin/psql -c \"SELECT 1 FROM pg_roles WHERE rolname='replication'\" -d delivery replication"
+  end
+
+  #Lay down primary config
+  template "/var/opt/delivery/postgresql/9.2/data/postgresql.conf" do
+    source "primary_postgresql.conf.erb"
+    owner 'chef-pgsql'
+    group 'chef-pgsql'
+    notifies :run, 'execute[restart_postgres]', :immediately
+  end
+
+  template "/var/opt/delivery/postgresql/9.2/data/pg_hba.conf" do
+    source "pg_hba.conf.erb"
+    owner 'chef-pgsql'
+    group 'chef-pgsql'
+    notifies :run, 'execute[restart_postgres]', :immediately
+  end
+
+  execute "restart_postgres" do
+    command "delivery-ctl restart postgresql"
+    action :nothing
+  end
+else
+  # Setup postgres.conf config and resolve.conf
+  template "/var/opt/delivery/postgresql/9.2/data/postgresql.conf" do
+    source "secondary_postgresql.conf.erb"
+    owner 'chef-pgsql'
+    group 'chef-pgsql'
+    notifies :run, 'execute[remove_recovery]', :immediately
+  end
+
+  ## Dirty Dirty hack
+  execute "remove_recovery" do
+    command "rm /var/opt/delivery/postgresql/9.2/data/recovery.conf"
+    action :nothing
+    only_if ::File.exist?("/var/opt/delivery/postgresql/9.2/data/recovery.conf")
+  end
+
+  template "/var/opt/delivery/postgresql/9.2/data/recovery.conf" do
+    source "recovery.conf.erb"
+    owner 'chef-pgsql'
+    group 'chef-pgsql'
+    notifies :run, 'execute[stop_postgres]', :immediately
+  end
+
+  execute "stop_postgres" do
+    command "delivery-ctl stop postgresql"
+    action :nothing
+    notifies :run, 'execute[pg_basebackup]', :immediately
+  end
+
+  execute "pg_basebackup" do
+    command "/opt/delivery/embedded/bin/pg_basebackup -h 33.33.33.11 -D /var/opt/delivery/postgresql/9.2/data/pg_basebackup -U replication --xlog-method=stream"
+    action :nothing
+    user 'chef-pgsql'
+    notifies :run, 'execute[activate_backup]', :immediately
+  end
+
+  execute "activate_backup" do
+    command "rsync -av --exclude pg_xlog --exclude postgresql.conf --exclude postgresql.pid /var/opt/delivery/postgresql/9.2/data/pg_basebackup/* /var/opt/delivery/postgresql/9.2/data"
+    action :nothing
+    notifies :run, 'execute[delete_backup]', :immediately
+  end
+
+  execute "delete_backup" do
+    command "rm -rf /var/opt/delivery/postgresql/9.2/data/pg_basebackup"
+    action :nothing
+    notifies :run, 'execute[start_postgres]', :immediately
+  end
+
+  execute "start_postgres" do
+    command "delivery-ctl start postgresql"
+    action :nothing
+  end
+end
